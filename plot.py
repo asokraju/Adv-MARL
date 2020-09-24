a70cfa52aa4d05e2b749945f368a55ef158db76e
asokraju <kkrishnachaitanya89@gmail.com>
2020-09-24 11:52:16 -0400

    plot function minor changes

--- plot.py
+++ plot.py
@@ -33,7 +33,7 @@ def net_reward(paths, episodes):
         #episodes_.append(np.shape(DATA['data'][0])[0])
         #episodes = min(episodes_)
         for epi in range(episodes):
-            _, _, rew = DATA['data'][0][epi][0][0]
+            _, _, rew, _ = DATA['data'][0][epi][0][0]
             for i in range(5):
                 r[i].append(rew[i].tolist())
         #print(np.array(r).shape)
@@ -44,16 +44,23 @@ def net_reward(paths, episodes):
 
 def net_compute_reward(paths, episodes):
     r_agent = [[] for _ in  range(5)]
+    #predicted_rewards_all = [[] for _ in  range(5)]
     for mat_path in iter(paths):
         r = [[] for _ in range(5)]
+        #predicted_returns = [[] for _ in range(5)]
         DATA = loadmat(mat_path)
         returns = []
         for epi in range(episodes):
-            states, _, _ = DATA['data'][0][epi][0][0]
+            states, _, _ ,predicted_rewards= DATA['data'][0][epi][0][0]
+            #print(np.shape(predicted_rewards))
+            #predicted_returns.append(np.array(predicted_rewards).reshape((5,1000)).sum(axis=1).tolist())
             r = [[dist(s1, s2) for s1,s2 in zip(s, des)] for s in states[0]]
             returns.append(np.sum(r, axis=0))
         returns = np.array(returns).T.tolist()
+        #print(np.shape(predicted_returns), np.shape(returns))
         [r_agent[i].append(rew) for i, rew in enumerate(returns)]
+        #[predicted_rewards_all[i].append(rew) for i, rew in enumerate(returns)]
+        #print(np.shape(r_agent))
     return r_agent
 
 
@@ -75,13 +82,14 @@ def plot_agent_rewards(paths, episodes, savefig_filename, set_format = 'pdf', co
         #print(mean, std)
         steps = np.arange(mean.size)
         #print(steps,mean)
-        # for run in range(df.shape[0]):
-        #     axes[i].plot(steps, df.iloc[run,:], color='000000', alpha=0.1)
+        for run in range(df.shape[0]):
+            axes[i].plot(steps, df.iloc[run,:], color='000000', alpha=0.1)
         axes[i].plot(steps, mean, color='b', alpha=1, label=label[i])
         axes[i].fill_between(steps, mean - std, mean + std,color='b', alpha=0.25)
         #axes[i].set_ylim(-6e6,0.1e6)
-        axes[i].set_ylim(-5e3,0e3)
-        axes[i].set_title('Agent-'+str(i), fontsize=15)
+        if compute:
+            axes[i].set_ylim(-5e3,0e3)
+        axes[i].set_title('Agent-'+str(i+1), fontsize=15)
         axes[i].set_label('Label via method')
         axes[i].legend()
     if savefig_filename is not None:
@@ -91,13 +99,13 @@ def plot_agent_rewards(paths, episodes, savefig_filename, set_format = 'pdf', co
         plt.show()
 ##%
 path = './Power-Converters/marl/results/matfiles/'
-files_1 = os.listdir(path + 'method_1/')
-files_2 = os.listdir(path + 'method_2/')
+files_1 = os.listdir(path + 'Adversory/')
+files_2 = os.listdir(path + 'Adversory_1/')
 
 #print(files)
-paths_1 = [path + 'method_1/' + file for file in files_1]
-paths_2 = [path + 'method_2/' + file for file in files_2]
-print(paths_1, files_2)
+paths_1 = [path + 'Adversory/'  + file for file in files_1]
+paths_2 = [path + 'Adversory_1/' + file for file in files_2]
+print(paths_1)
 print(paths_2)
-plot_agent_rewards(paths = paths_1 + paths_2, episodes = 100, savefig_filename=path+'plot.pdf', set_format = 'pdf', compute = True)
+plot_agent_rewards(paths = paths_1 + paths_2, episodes = 200, savefig_filename=path+'plot.pdf', set_format = 'pdf', compute = True)
 #plot_agent_rewards(paths_ddpg, episodes = 200)
